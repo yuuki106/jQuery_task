e934211335ccfbfabea825653349663acc8e5349
prs-teacher <corporation.prs.training@gmail.com>
2021-02-01 22:09:39 +0900

    first commit

---
+++ q8/work/js/app.js
@@ -0,0 +1,11 @@
+// API
+// const settings = {
+//   "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+//   "method": "GET",
+// }
+// $.ajax(settings).done(function (response) {
+//   const result = response['@graph'];
+//   displayResult(result)
+// }).fail(function (err) {
+//   displayError(err)
+// });
