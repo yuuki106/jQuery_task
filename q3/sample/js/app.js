ecdef0f56be0fef90055ec07da696c45afc9279c
prs-teacher <corporation.prs.training@gmail.com>
2021-03-21 14:06:35 +0900

    バグ修正

--- q3/sample/js/app.js
+++ q3/sample/js/app.js
@@ -1,1 +1,1 @@
-$(function () { $(".drawer_button").click(function () { $(this).toggleClass("active"); $(".drawer_bg").fadeToggle(); $("nav").toggleClass("open") }); $(".drawer_bg").click(function () { $(this).fedeOut(); $(".drawer_button").removeClass("active"); $("nav").removeClass("open") }) })
+$(function () { $(".drawer_button").click(function () { $(this).toggleClass("active"); $(".drawer_bg").fadeToggle(); $("nav").toggleClass("open") }); $(".drawer_bg").click(function () { $(this).hide(); $(".drawer_button").removeClass("active"); $("nav").removeClass("open") }) });
