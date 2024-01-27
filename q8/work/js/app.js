90bdae854d8a0e68d8e894ffdfa23ea434756cf0
Honda Yuuki <yuuki106byzance@gmail.com>
2024-01-27 15:53:00 +0900

    修正(4)

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,16 +1,17 @@
 
 $(function () {
 
-  function ajaxFail() {//「ajaxFail」という関数を作り、通信失敗時の処理を書いていく。
-
-    //通信失敗時に表示させたいアナウンスを変数「errorText」に代入。なお表示させたいアナウンスには「message」クラスをつける。
-    const errorText = `
+  function ajaxFail(data) {//「ajaxFail」という関数を作り、通信失敗時の処理を書いていく。
+    if ($.isEmptyObject(data)) {//もし受け取ったdataが空だった場合、
+      //通信失敗時に表示させたいアナウンスを変数「errorText」に代入。なお表示させたいアナウンスには「message」クラスをつける。
+      const errorText = `
         <div class="message">
           <p>正常に通信できませんでした。<br>
           インターネットの接続の確認をしてください。</p>
         </div>
       `;
-    $(".inner").prepend(errorText);//「errorText」を「inner」へ追加。
+      $(".lists").empty().append(errorText);//メッセージの重複表示を防ぐ為、「empty()」で削除してからerrorTextを追加
+    }
   };
 
   function ajaxDone(data) {//「ajaxDone」という関数を作り、通信成功時の処理を書いていく。
@@ -22,27 +23,36 @@ $(function () {
             別のキーワードで検索して下さい。</p>
           </div>
         `;
-        $(".inner").children(".message").remove();//notFoundTextの重複表示を防ぐ為、一旦削除。
-        $(".inner").prepend(notFoundText);//innerにnotFoundTextを追加
+      $(".lists").empty().append(notFoundText);////メッセージの重複表示を防ぐ為、「empty()」で削除してからnotFoundTextを追加
     } else {
+
       //ajax通信で受け取った「data」をeachで繰り返し処理。
       $.each(data["@graph"][0].items, function (i) {
+        let bookCreator = data["@graph"][0].items[i]["dc:creator"];//受け取った作者名を「bookCreator」へ代入。
+        let bookPublisher = data["@graph"][0].items[i]["dc:publisher"];//受け取った出版社名を「bookPublisher」へ代入。
+        let bookTitle = data["@graph"][0].items[i].title//受け取ったタイトル名を「bookTitle」へ代入。
 
-        //作者が不明（undefined）な場合は、「作者不明」と、作者がわかる場合は作者名を表示するようcreatorsに代入。
-        const creatorsFunc = `${data["@graph"][0].items[i]["dc:creator"]}`;
-        const creators = (creatorsFunc === "undefined") ? "作者不明" : creatorsFunc;
+        if (typeof bookCreator === "undefined") {//もしbookCreatorがundefinedの場合、
+          bookCreator = "作者不明";//bookCreatorに作者不明を代入
+        }
+        if (typeof bookPublisher === "undefined") {//もしbookPublisherがundefinedの場合、
+          bookPublisher = "出版社不明";//bookPublisherに出版社不明を代入
+        }
+        if (typeof bookTitle === "undefined") {//もしbookTitleがundefinedの場合、
+          bookTitle = "タイトル不明";//bookTitleにタイトル不明を代入
+        }
 
         //受け取ったdata（本のタイトル、作者、出版社、リンク）をそれぞれ表示したい為、「html」に代入。lists-itemの中にlist-innerを作成。そのさらに中には本のタイトル、作者、出版社、リンク表示用の各要素を作成。
         const html = `
-        <li class="lists-item">
-          <div class="list-inner">
-            <p>タイトル：${data["@graph"][0].items[i].title}</p>
-            <p>作者：${creators}</p>
-            <p>出版社：${data["@graph"][0].items[i]["dc:publisher"]}</p>
-            <a href="${data["@graph"][0].items[i].link["@id"]}" target="_blank">書籍情報</a>
-          </div>
-        </li>
-      `;
+          <li class="lists-item">
+            <div class="list-inner">
+              <p>タイトル：${bookTitle}</p>
+              <p>作者：${bookCreator}</p>
+              <p>出版社：${bookPublisher}</p>
+              <a href="${data["@graph"][0].items[i].link["@id"]}" target="_blank">書籍情報</a>
+            </div>
+          </li>
+        `;
         $(".lists").prepend(html);//listsに「html」を追加。
       })
     }
@@ -52,21 +62,19 @@ $(function () {
 
   //リセットボタンを押したら、
   $(".reset-btn").on("click", function () {
-    $(".inner").children().empty();//「inner」の子要素を消す
+    $(".lists").empty();//listsの検索結果一覧を消す。
     $("#search-input").val("");//入力された検索ワードを消す。
     pageCount = 1;//pageCountを初期値にする
   })
 
   let pageCount = 1;//pageCountを１と定義。
-  let searchWord = " ";//searchWordを空の状態で定義。
+  let searchWord = "";//searchWordを空の状態で定義。
 
   //search-btnをクリックしたら、
   $(".search-btn").on("click", function () {
-
     //既にsearchWordに入力されている検索ワードと現在の検索ワードが異なる場合、
     if (searchWord !== $("#search-input").val()) {
-
-      $(".inner").children().empty();//「inner」の子要素を消す
+      $(".lists").empty();//listsの検索結果一覧を消す。
       searchWord = $("#search-input").val();//検索ワードをsearchWordに代入。
       pageCount = 1;//pageCountを初期値にする
     }
