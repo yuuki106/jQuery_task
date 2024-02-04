437316a09a4c775a66a24c6f63ede589c61e1756
Honda Yuuki <yuuki106byzance@gmail.com>
2024-02-04 22:30:23 +0900

    修正(9)

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,10 +1,10 @@
 
 $(function () {
 
-  function ajaxFail(data) {//「ajaxFail」という関数を作り、通信失敗時の処理を書いていく。
-    if (data.status === 0) {//もしdata.statusが0だった場合、
+  function ajaxFail(failData) {//「ajaxFail」という関数を作り、通信失敗時の処理を書いていく。
+    console.log(failData.status);
+    if (failData.status === 0) {//もしfailData.statusが0だった場合、
       //通信失敗時に表示させたいアナウンスを変数「errorText」に代入。なお表示させたいアナウンスには「message」クラスをつける。
-      console.log
       const errorText = `
         <div class="message">
           <p>正常に通信できませんでした。<br>
@@ -12,12 +12,20 @@ $(function () {
         </div>
       `;
       $(".lists").empty().append(errorText);//メッセージの重複表示を防ぐ為、「empty()」で削除してからerrorTextを追加
-
-    }
+    } else {//statusが0以外のエラーが発生した場合、
+      //表示させたいアナウンスを変数「othersErrorText」に代入。なお表示させたいアナウンスには「message」クラスをつける。
+      const othersErrorText = `
+        <div class="message">
+          <p>予期せぬエラーが起きました。<br>
+          再読み込みを行ってください。</p>
+        </div>
+      `;
+      $(".lists").empty().append(othersErrorText);//メッセージの重複表示を防ぐ為、「empty()」で削除してからothersErrorTextを追加
+    };
   };
 
-  function ajaxDone(data) {//「ajaxDone」という関数を作り、通信成功時の処理を書いていく。
-    if ($.isEmptyObject(data["@graph"][0].items)) {//もし受け取ったdataが空だった場合、
+  function ajaxDone(doneData) {//「ajaxDone」という関数を作り、通信成功時の処理を書いていく。
+    if ($.isEmptyObject(doneData["@graph"][0].items)) {//もし受け取ったdataが空だった場合、
       //書籍が見つからない場合のアナウンスを「notFoundText」に代入。
       const notFoundText = `
           <div class="message">
@@ -29,10 +37,10 @@ $(function () {
     } else {
 
       //ajax通信で受け取った「data」をeachで繰り返し処理。
-      $.each(data["@graph"][0].items, function (i) {
-        const bookCreator = data["@graph"][0].items[i]["dc:creator"] === undefined ? "作者不明" : data["@graph"][0].items[i]["dc:creator"];//data["@graph"][0].items[i]["dc:creator"]がundefinedの場合は「作者不明」を代入。そうでない場合はdata["@graph"][0].items[i]["dc:creator"]を代入
-        const bookPublisher = data["@graph"][0].items[i]["dc:publisher"] === undefined ? "出版社" : data["@graph"][0].items[i]["dc:publisher"];//data["@graph"][0].items[i]["dc:publisher"]がundefinedの場合は「出版社不明」を代入。そうでない場合はdata["@graph"][0].items[i]["dc:publisher"]を代入
-        const bookTitle = data["@graph"][0].items[i].title === undefined ? "タイトル不明" : data["@graph"][0].items[i].title//data["@graph"][0].items[i].titleがundefinedの場合は「タイトル不明」を代入。そうでない場合はdata["@graph"][0].items[i].titleを代入
+      $.each(doneData["@graph"][0].items, function (i) {
+        const bookCreator = doneData["@graph"][0].items[i]["dc:creator"] === undefined ? "作者不明" : doneData["@graph"][0].items[i]["dc:creator"];//data["@graph"][0].items[i]["dc:creator"]がundefinedの場合は「作者不明」を代入。そうでない場合はdata["@graph"][0].items[i]["dc:creator"]を代入
+        const bookPublisher = doneData["@graph"][0].items[i]["dc:publisher"] === undefined ? "出版社" : doneData["@graph"][0].items[i]["dc:publisher"];//data["@graph"][0].items[i]["dc:publisher"]がundefinedの場合は「出版社不明」を代入。そうでない場合はdata["@graph"][0].items[i]["dc:publisher"]を代入
+        const bookTitle = doneData["@graph"][0].items[i].title === undefined ? "タイトル不明" : doneData["@graph"][0].items[i].title//data["@graph"][0].items[i].titleがundefinedの場合は「タイトル不明」を代入。そうでない場合はdata["@graph"][0].items[i].titleを代入
 
         //受け取ったdata（本のタイトル、作者、出版社、リンク）をそれぞれ表示したい為、「html」に代入。lists-itemの中にlist-innerを作成。そのさらに中には本のタイトル、作者、出版社、リンク表示用の各要素を作成。
         const html = `
@@ -41,7 +49,7 @@ $(function () {
               <p>タイトル：${bookTitle}</p>
               <p>作者：${bookCreator}</p>
               <p>出版社：${bookPublisher}</p>
-              <a href="${data["@graph"][0].items[i].link["@id"]}" target="_blank">書籍情報</a>
+              <a href="${doneData["@graph"][0].items[i].link["@id"]}" target="_blank">書籍情報</a>
             </div>
           </li>
         `;
@@ -73,13 +81,13 @@ $(function () {
 
     //ajax通信の準備を行う。
     $.ajax({
-      type: "GET",//データを「受け取る」に設定。
       url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&p=${pageCount}&format=json&count=20`,//通信先のURLを設定。
-    }).done(function (data) {//通信成功時、
-      ajaxDone(data)//ajaxDone関数を呼び出す
+      type: "GET",//データを「受け取る」に設定。
+    }).done(function (doneData) {//通信成功時、
+      ajaxDone(doneData)//ajaxDone関数を呼び出す
 
-    }).fail(function (date) {//通信失敗時。
-      ajaxFail(date);//ajaxFail関数を呼び出す
+    }).fail(function (failData) {//通信失敗時。
+      ajaxFail(failData)//ajaxFail関数を呼び出す
     })
   });
 })
